522d1ebde071ab746d8ad21b051df523aa175684
Hao-Xuan <Hao-Xuan@users.noreply.github.com>
2026-01-29 10:07:13 -0700

    finished notes for section 5

--- gitNotes.c
+++ gitNotes.c
@@ -1,5 +1,4 @@
 /*
-
 Git local workflow file states:
 
 1 - untracked
@@ -11,8 +10,8 @@ Git local workflow file states:
 2 - tracked, not changed
     - file has not been modified since last commit
     - modifying moves file to state 3
-    - committing moves file to state 5
-        - ex: git commit -m "detailed commit message"
+    - committing moves file to state 5, skipping stage 4
+        - ex: git commit -a -m "detailed commit message"
  
 3 - tracked, changed
     - file has been modified since last commit
@@ -20,16 +19,19 @@ Git local workflow file states:
         - ex: git add gitNotes.c
     - restoring moves file to state 2, reverting all changes since last commit
         - ex: git restore gitNotes.c
+    - committing moves file to state 5, skipping stage 4
+        - ex: git commit -a -m "detailed commit message"
 
 4 - staged
     - file added to repository
-    - modifying creates a revised file in state 3
-    - committing moves file to state 5
-        - ex: git commit -m "detailed commit message"
     - restoring moves file to state 1
         - ex: git restore --staged gitNotes.c
-    
+    - modifying creates a revised file in state 3
+    - committing moves file to state 5
+        - ex: git commit -m "detailed commit message"        
 
 5 - committed
+    - file snapshot saved to repository and logged
+        - ex: git log
 
 */
