0f376b51769268fda8cdcd588c11a600a2baf98b
Hao-Xuan <Hao-Xuan@users.noreply.github.com>
2026-01-29 09:59:17 -0700

    added notes on file states in gitNotes.c

---
+++ gitNotes.c
@@ -0,0 +1,35 @@
+/*
+
+Git local workflow file states:
+
+1 - untracked
+    - file present in working directory
+    - not added to repository
+    - adding moves file to state 4
+        - ex: git add gitNotes.c
+
+2 - tracked, not changed
+    - file has not been modified since last commit
+    - modifying moves file to state 3
+    - committing moves file to state 5
+        - ex: git commit -m "detailed commit message"
+ 
+3 - tracked, changed
+    - file has been modified since last commit
+    - adding moves file to state 4
+        - ex: git add gitNotes.c
+    - restoring moves file to state 2, reverting all changes since last commit
+        - ex: git restore gitNotes.c
+
+4 - staged
+    - file added to repository
+    - modifying creates a revised file in state 3
+    - committing moves file to state 5
+        - ex: git commit -m "detailed commit message"
+    - restoring moves file to state 1
+        - ex: git restore --staged gitNotes.c
+    
+
+5 - committed
+
+*/
